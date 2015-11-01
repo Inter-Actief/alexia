a89e367cd06f6ccaefea6c5e087b67399f20c68a
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2015-11-01 22:10:27 +0100

    auth: Utility to get or create user from authentication data table

---
+++ utils/auth/backends/__init__.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+
+from apps.organization.models import Profile, AuthenticationData
+
+
+def get_or_create_user(backend, username):
+    try:
+        authentication_data = AuthenticationData.objects.get(backend=backend, username=username)
+        return authentication_data.user, False
+    except AuthenticationData.DoesNotExist:
+        user = User(username=username)
+        user.set_unusable_password()
+        user.save()
+
+        data = AuthenticationData(user=user, backend=backend, username=username.lower())
+        data.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return user, True
