afb4e246e904c4586ef3df13eca019350b481b35
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2015-11-01 22:06:35 +0100

    auth: Add an LDAP backend using the authentication data table

---
+++ utils/auth/backends/ldap.py
@@ -0,0 +1,8 @@
+from django_auth_ldap.backend import LDAPBackend
+from utils.auth.backends import get_or_create_user
+
+
+class MultiLDAPBackend(LDAPBackend):
+    def get_or_create_user(self, username, ldap_user):
+        backend = self.__module__ + "." + self.__class__.__name__
+        return get_or_create_user(backend, username)
