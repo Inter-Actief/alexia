5901b26905bd4fd38b715b964d784f8080fec4aa
Jelte Zeilstra <zeilstraj@inter-actief.net>
2015-11-24 00:30:30 +0100

    organization: Created admin interfaces for AuthenticationData.

--- apps/organization/admin.py
+++ apps/organization/admin.py
@@ -6,12 +6,18 @@ from apps.billing.admin import AuthorizationInline, RfidCardInline, \
     PriceGroupInline, ProductGroupInline
 from apps.scheduling.admin import AvailabilityInline, \
     StandardReservationInline
-from .models import Location, Profile, Organization, Membership, Certificate
+from .models import Location, AuthenticationData, Profile, Organization, Membership, Certificate
 
 
 class MembershipInline(admin.TabularInline):
     model = Membership
     extra = 1
+
+
+class AuthenticationDataInline(admin.TabularInline):
+    model = AuthenticationData
+    extra = 1
+    fields = ('backend', 'username', 'additional_data')
 
 
 class ProfileInline(admin.StackedInline):
@@ -22,7 +28,13 @@ class ProfileInline(admin.StackedInline):
 
 
 class UserAdmin(UserAdmin):
-    inlines = (ProfileInline, MembershipInline, AuthorizationInline, RfidCardInline)
+    inlines = (AuthenticationDataInline, ProfileInline, MembershipInline, AuthorizationInline, RfidCardInline)
+
+
+class AuthenticationDataAdmin(admin.ModelAdmin):
+    list_display = ('username', 'backend', 'user')
+    list_filter = ('backend', )
+    search_fields = ('username', 'backend', 'user__username', 'user__first_name', 'user__last_name')
 
 
 class OrganizationAdmin(admin.ModelAdmin):
@@ -46,6 +58,7 @@ class CertificateAdmin(admin.ModelAdmin):
 admin.site.unregister(User)
 
 admin.site.register(User, UserAdmin)
+admin.site.register(AuthenticationData, AuthenticationDataAdmin)
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(Location, LocationAdmin)
 admin.site.register(Certificate, CertificateAdmin)
