8eb97f698bd3afa12272e44f5737fd9671b63aa1
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2016-02-07 16:26:28 +0100

    organization, utils: Cleanup some unused code

--- utils/auth/decorators.py
+++ utils/auth/decorators.py
@@ -2,7 +2,6 @@ from functools import wraps
 
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseBadRequest
-from django.contrib.auth.views import redirect_to_login
 
 
 def ajax_required(f):
@@ -48,20 +47,6 @@ def manager_required(f):
         if not request.user.is_authenticated() or not request.user.is_superuser and (
                 not request.organization or not request.user.profile.is_manager(request.organization)):
             raise PermissionDenied
-        return f(request, *args, **kwargs)
-
-    return wrap
-
-
-def foundation_manager_required(f):
-    """Checks whether the current user is at least a foundation manager or is superuser.
-    """
-
-    @wraps(f)
-    def wrap(request, *args, **kwargs):
-        if not request.user.is_authenticated() or not request.user.is_superuser and \
-                not request.user.profile.is_foundation_manager():
-            return redirect_to_login(request)
         return f(request, *args, **kwargs)
 
     return wrap
