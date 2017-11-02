23c29c4964286fc2ca8fb3a957a6e7810edb9d17
Kevin Alberts <albertskja@inter-actief.net>
2017-11-02 23:01:37 +0100

    Fix AttributeError: 'AnonymousUser' object has no attribute 'membership_set'

--- alexia/template/context_processors.py
+++ alexia/template/context_processors.py
@@ -15,13 +15,21 @@ def permissions(request):
         return {'is_tender': True, 'is_planner': True, 'is_manager': True, 'is_foundation_manager': True}
 
     try:
-        membership = request.user.membership_set.get(organization=request.organization)
-        return {
-            'is_tender': membership.is_tender,
-            'is_planner': membership.is_planner,
-            'is_manager': membership.is_manager,
-            'is_foundation_manager': request.user.profile.is_foundation_manager,
-        }
+        if hasattr(request.user, "membership_set"):
+            membership = request.user.membership_set.get(organization=request.organization)
+       	    return {
+                'is_tender': membership.is_tender,
+                'is_planner': membership.is_planner,
+                'is_manager': membership.is_manager,
+                'is_foundation_manager': request.user.profile.is_foundation_manager,
+            }
+        else:
+            return {
+                'is_tender': False,
+                'is_planner': False,
+                'is_manager': False,
+                'is_foundation_manager': False,
+            }
     except Organization.DoesNotExist:
         return {
             'is_tender': False,
