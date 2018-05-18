c4071f523b1dca7f10884a0e7429ccff5704018f
Kevin Alberts <kevin@kevinalberts.nl>
2018-05-18 16:07:30 +0200

    Expand Alexia API to include some methods for GDPR-compliance (#57)
    * Expand Alexia API to include some methods for GDPR-compliance
    
    These methods will mostly be used by Inter-Actief's website to facilitate that users are able to request a data-export of all their user data. Because this also includes personal data stored in other systems, and we want to provide a single place to create this export, we need a way to export this information from Alexia.
    
    * Optional arguments aren't supported

--- alexia/api/v1/methods/billing.py
+++ alexia/api/v1/methods/billing.py
@@ -4,6 +4,7 @@ from jsonrpc import jsonrpc_method
 from alexia.api.decorators import manager_required
 from alexia.api.exceptions import InvalidParamsError, ObjectNotFoundError
 from alexia.apps.billing.models import Order
+from alexia.auth.backends import RADIUS_BACKEND_NAME
 
 from ..common import format_order
 from ..config import api_v1_site
@@ -141,6 +142,89 @@ def order_get(request, order_id):
         raise ObjectNotFoundError
 
     return format_order(order)
+
+
+@jsonrpc_method('order.list(radius_username=String) -> Array', site=api_v1_site, safe=True, authenticated=True)
+@manager_required
+def order_list(request, radius_username):
+    """
+    Retrieve a list of orders for the currently selected organization.
+
+    Required user level: Manager
+
+    Povide radius_username to select only orders made by the provided user.
+
+    Returns an array of orders.
+
+    radius_username -- RADUIS username to search for.
+
+    Example return value:
+    [
+        {
+            "purchases": [
+                {
+                    "price": "1.00",
+                    "product": {"name": "Coca Cola"},
+                    "amount": 2
+                }, {
+                    "price": "0.50",
+                    "product": {"name": "Grolsch"},
+                    "amount": 1
+                }
+            ],
+            "synchronized": false,
+            "event": {
+                "id": 4210,
+                "name": "Testborrel"
+            },
+            "placed_at": "2015-03-11T15:24:06+00:00",
+            "id": 1255,
+            "rfid": "02,06:65:74:49",
+            "authorization": {
+                "id": 1,
+                "end_date": null,
+                "start_date": "2014-09-21T14:16:06+00:00",
+                "user": "s0000000"
+            }
+        },
+        {
+            "purchases": [
+                {
+                    "price": "1.50",
+                    "product": {"name": "Grolsch"},
+                    "amount": 3
+                }
+            ],
+            "synchronized": true,
+            "event": {
+                "id": 4210,
+                "name": "Testborrel"
+            },
+            "placed_at": "2015-03-11T16:47:21+00:00",
+            "id": 1271,
+            "rfid": "02,06:65:74:49",
+            "authorization": {
+                "id": 1,
+                "end_date": null,
+                "start_date": "2014-09-21T14:16:06+00:00",
+                "user": "s0000000"
+            }
+        }
+    ]
+    """
+    result = []
+    orders = Order.objects.filter(event__organizer=request.organization)
+
+    if radius_username is not None:
+        orders = orders.filter(authorization__user__authenticationdata__backend=RADIUS_BACKEND_NAME,
+                               authorization__user__authenticationdata__username=radius_username)
+
+    orders = orders.select_related('event', 'authorization')
+
+    for order in orders:
+        result.append(format_order(order))
+
+    return result
 
 
 @jsonrpc_method('order.marksynchronized(order_id=Number) -> Boolean', site=api_v1_site, authenticated=True)
