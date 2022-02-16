a66c1c585797a722263e16ace892894d8cc4028f
Bram van Dartel <b.vandartel@student.utwente.nl>
2022-02-16 12:43:39 +0100

    Also allow managers to access the matrix

--- alexia/apps/scheduling/views.py
+++ alexia/apps/scheduling/views.py
@@ -166,7 +166,8 @@ class EventMatrixView(TemplateView):
     template_name = 'scheduling/event_matrix.html'
 
     def get_context_data(self, **kwargs):
-        if hasattr(self.request.user, 'profile') and self.request.user.profile.is_tender(self.request.organization):
+        if hasattr(self.request.user, 'profile') and (self.request.user.profile.is_tender(self.request.organization)
+                                                      or self.request.user.profile.is_manager(self.request.organization)):
             events = self.get_events()
             context = super(EventMatrixView, self).get_context_data(**kwargs)
             context['events'] = events
