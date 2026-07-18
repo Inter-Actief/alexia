cc033b8c34cfe4fba57bc0f7acf893e428759f3c
Mihai Pop <pop.mihai2410@gmail.com>
2026-07-18 14:32:08 +0200

    resize date and time picker so they are inline

--- alexia/forms/widgets.py
+++ alexia/forms/widgets.py
@@ -2,13 +2,13 @@ from django import forms
 
 
 class NativeSplitDateTimeWidget(forms.SplitDateTimeWidget):
-    """SplitDateTimeWidget using the browser's native date and time inputs."""
+    """SplitDateTimeWidget using the browser's native date and time inputs, rendered inline and compact."""
 
     def __init__(self, attrs=None):
         super(NativeSplitDateTimeWidget, self).__init__(
             attrs=attrs,
-            date_attrs={'type': 'date'},
-            time_attrs={'type': 'time'},
+            date_attrs={'type': 'date', 'class': 'd-inline-block me-2', 'style': 'width: 10rem'},
+            time_attrs={'type': 'time', 'class': 'd-inline-block', 'style': 'width: 7rem'},
             date_format='%Y-%m-%d',
             time_format='%H:%M',
         )
